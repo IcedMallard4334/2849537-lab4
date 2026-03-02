d1ef5fe4d9ac8e3ccf623fcd110e9aebe7169ee6
IcedMallard4334 <hyginusvictor11@gmail.com>
2026-03-02 15:55:20 +0200

    Initial Commit: File Structure

---
+++ script.js
@@ -0,0 +1,19 @@
+async function searchCountry(countryName) {
+    try {
+        // Show loading spinner
+        // Fetch country data
+        // Update DOM
+        // Fetch bordering countries
+        // Update bordering countries section
+    } catch (error) {
+        // Show error message
+    } finally {
+        // Hide loading spinner
+    }
+}
+
+// Event listeners
+document.getElementById('search-btn').addEventListener('click', () => {
+    const country = document.getElementById('country-input').value;
+    searchCountry(country);
+});
